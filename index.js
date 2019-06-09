8ae41a356b0e7edde54a4209ec18a3fb750915da
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-09 19:48:51 +0900

    elseif,switchを学習

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-// ボタンクリック時の動作
-document.getElementById('btnClose').onclick = function(){
-  if(confirm('閉じてよろしいでしょうか？')){
-    close();
-  }else{
-    if(confirm('本当にいいんですか？')){
-    alert('閉じるのを中止しました');
-  }}
+// 選択時の挙動
+document.getElementById('age').onchange = function(){
+  var age = document.getElementById('age').value;
+
+// switchを使った条件式
+  switch(age){
+    case '10':
+      alert('10歳以下の方は、半額です');
+      break;
+    case '30':
+      alert('11-30歳の方は割引はありません');
+      break;
+    case '50':
+      alert('31-50歳の方は割引はありません');
+      break;
+    case '51':
+      alert('51歳以上の方は30%の割引です');
+      break;
+  }
+
+// if-elseifを使った条件式
+  // if(age === '10'){
+  //   alert('10歳以下の方は、半額です');
+  // } else if
+  //   (age === '30'){
+  //   alert('11-30歳の方は割引はありません');
+  // } else if
+  //   (age === '50'){
+  //   alert('31-50歳の方は割引はありません');
+  // } else if
+  //   (age === '51'){
+  //   alert('51歳以上の方は30%の割引です');
+  // }
 }
