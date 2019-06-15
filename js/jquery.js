30c6a478b756ca5aea1acd674aca3dbf343d02e6
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 01:13:56 +0900

    updated the jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,5 @@
-jQuery('#btnClose').bind('click', function(){
-  if (confirm('閉じてよろしいですか？')) {
-    window.close();
-  }else{
-    alert('閉じるのを中止しました');
-  }
-});
+// # [content]IDのp要素を以下のhtmlに書き換え
+jQuery('#content p').html('jQueryで内容を書き換えます');
+// [content p]に対して、cssメソッドでプロパティと色を指定する
+// cssの内容を後から変更している
+jQuery('#content p').css('color', '#f00');
