9b864ddcf45b3b601104310a5b3cefd66509f261
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 21:22:35 +0900

    hoverを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,6 @@
-// thisとattrを用いて、マウスが振れた/離れたときにsrc属性の属性値を_off/_onに切り替える
-$('nav img')
-.bind('mouseenter', function(){
+// hoverとthisとattrを用いて、マウスが振れた/離れたときにsrc属性の属性値を_off/_onに切り替える
+$('nav img').hover(function(){
   $(this).attr('src', $(this).attr('src').replace('_off', '_on'));
-})
-.bind('mouseleave', function(){
+}, function(){
   $(this).attr('src', $(this).attr('src').replace('_on', '_off'));
 });
