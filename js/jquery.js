27e22608ab2636344c166363ce9d4e6c942ae8f3
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 21:17:02 +0900

    replaceを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,8 @@
-// nav imgセレクタに対してマウスが触れた時にattrで指定した内容のgifに変化する
+// thisとattrを用いて、マウスが振れた/離れたときにsrc属性の属性値を_off/_onに切り替える
 $('nav img')
 .bind('mouseenter', function(){
-  $(this).attr('src', 'img/btn_company_on.gif');
+  $(this).attr('src', $(this).attr('src').replace('_off', '_on'));
 })
 .bind('mouseleave', function(){
-  $(this).attr('src', 'img/btn_company_off.gif');
+  $(this).attr('src', $(this).attr('src').replace('_on', '_off'));
 });
