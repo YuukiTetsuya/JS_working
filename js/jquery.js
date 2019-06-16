c79776993e837cdb65fa4071aaa65d9f668ec9f2
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 18:46:52 +0900

    slideToggleを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,13 @@
 // toolTitleをクリック時のdisplayを表示非表示に設定
 
 $('#toolTitle').bind('click', function(){
-  if($('#toolContent').css('display') !== 'none'){
-    $('#toolContent').slideUp();
-  }else{
-    $('#toolContent').slideDown();
-  }
+  $('#toolContent').slideToggle();
 });
+
+// $('#toolTitle').bind('click', function(){
+//   if($('#toolContent').css('display') !== 'none'){
+//     $('#toolContent').slideUp();
+//   }else{
+//     $('#toolContent').slideDown();
+//   }
+// });
