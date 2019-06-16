8b1c5965578930bc0fd475b64cd792621eb0945b
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 17:53:33 +0900

    アコーディオンメニューを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,19 @@
-// 初期状態でtoolContentを表示し、slideDownを表示させる
-$('#toolContent').hide();
-$('#toolTitle').bind('click', function(){
-  $('#toolContent').slideDown('slow');
+// 各スライド文字を初期状態/クリック時に非表示にし、セレクタがクリックされたときのみ
+// 各セレクタをスライドダウンさせる
+
+$('.toolContent').hide();
+
+$('#linkFile').bind('click', function(){
+  $('.toolContent').hide();
+  $('#menuFile').slideDown('slow');
+});
+
+$('#linkEdit').bind('click', function(){
+  $('.toolContent').hide();
+  $('#menuEdit').slideDown('slow');
+});
+
+$('#linkView').bind('click', function(){
+  $('.toolContent').hide();
+  $('#menuView').slideDown('slow');
 });
