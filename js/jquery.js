f253f887238dfbd42675519a6bb8764b32e0423a
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 17:56:10 +0900

    this,visibleを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,19 +1,11 @@
 // 各スライド文字を初期状態/クリック時に非表示にし、セレクタがクリックされたときのみ
 // 各セレクタをスライドダウンさせる
 
+// thisを用いた省略コード
+
 $('.toolContent').hide();
 
-$('#linkFile').bind('click', function(){
-  $('.toolContent').hide();
-  $('#menuFile').slideDown('slow');
-});
-
-$('#linkEdit').bind('click', function(){
-  $('.toolContent').hide();
-  $('#menuEdit').slideDown('slow');
-});
-
-$('#linkView').bind('click', function(){
-  $('.toolContent').hide();
-  $('#menuView').slideDown('slow');
+$('.toolTitle a').bind('click', function(){
+  $('.toolContent:visible').slideUp();
+  $($(this).attr('href')).slideDown();
 });
