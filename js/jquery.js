2e841ebea6bbec6a13a66bdaaa0a4256533867ea
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 20:16:33 +0900

    jquery selecta update

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,8 @@
 // toolTitleをクリック時のdisplayを表示非表示に設定
 
-$('#toolTitle').bind('click', function(){
+$('#toolTitle ').bind('click', function(){
   $('#toolContent').slideToggle();
+  $('#toolContent :last').html('こんにちは');
 });
 
 // $('#toolTitle').bind('click', function(){
