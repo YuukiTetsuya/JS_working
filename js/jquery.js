1585679e4db914858014b5901da4dff3f6dcf2ad
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 21:36:05 +0900

    preload処理を追加

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,8 @@
+//プリロード処理(下記画像をあらかじめ読み込む)
+$('<img>').attr('src', 'img/btn_company_on.gif');
+$('<img>').attr('src', 'img/btn_product_on.gif');
+$('<img>').attr('src', 'img/btn_product_on.gif');
+
 // hoverとthisとattrを用いて、マウスが振れた/離れたときにsrc属性の属性値を_off/_onに切り替える
 $('nav img').hover(function(){
   $(this).attr('src', $(this).attr('src').replace('_off', '_on'));
