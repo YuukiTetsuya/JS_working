cee0c7fa0c83ea3dfcc51436e9ef59ff0777e50a
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-16 20:52:40 +0900

    mouseenter,mouseleaveを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,2 +1,7 @@
 // nav imgセレクタに対して、attrでsrcを限定して内容をonに変化される
-$('nav img').attr('src', 'img/btn_company_on.gif');
+$('nav img').bind('mouseenter', function(){
+  $('nav img').attr('src', 'img/btn_company_on.gif');
+});
+$('nav img').bind('mouseleave', function(){
+  $('nav img').attr('src', 'img/btn_company_off.gif');
+})
