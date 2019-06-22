bd6adab2a1f8bbafb4069ed4e5a1072c07e2545c
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 16:29:12 +0900

    valueが空の時の処理を修正

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,4 @@
-// input要素のname属性値が未記入である場合、コントロールをログする
+// input要素のname属性値からフォーカスが外れた時に未記入である場合、コントロールをログする
 $('input[name=myname]').bind('blur', function(){
   if($(this).val() == ""){
   console.log('未記入です')
