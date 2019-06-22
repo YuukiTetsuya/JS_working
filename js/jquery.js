ef5eef9e05c63461bd19742fb598348950890841
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 18:29:15 +0900

    isNaNを学習

--- js/jquery.js
+++ js/jquery.js
@@ -2,7 +2,7 @@
 $('input[name=myname]')
 .bind('blur', function(){
   if($(this).val() === ''){
-// input要素にerrorクラスをaddし、inputの親要素ddの中にappendでpタグを追加
+  // input要素にerrorクラスをaddし、inputの親要素ddの中にappendでpタグを追加
   $(this).addClass('error').parent()
   .append('<p class="error">&#x26a0;この項目は必ずご記入ください</p>');
   }
@@ -20,8 +20,24 @@ $('input[name=gender]').parents('dd')
 .append('<p class="error">&#x26a0;この項目は必ずお選びください</p>')
 .end()
 .bind('change', function(){
-// ラジオボタンが選択されていれば、エラーメッセージをremoveする
+  // ラジオボタンが選択されていれば、エラーメッセージをremoveする
   if($('input[name=gender]:checked').val() !== ''){
     $(this).parents('dd').find('.error').remove();
   }
 });
+
+// input要素に対してフォーカスが外れた場合、親要素の中の.errorをremoveする
+$('input[name=age]')
+  .bind('blur', function(){
+    $(this).parent().find('.error').remove();
+    // input要素の入力値が数値でない場合、.errorをaddし、親要素に対してp要素をappendする
+    if(isNaN($(this).val())){
+      $(this).addClass('error').parent().append('<p class="error">&#x26a0;この項目は数値でご記入ください</p>');
+    }
+  })
+  // input要素に対してフォーカスされた時、p要素が存在すればinput要素の.errorをremove&p要素(エラーメッセージ)をremoveする
+  .bind('focus', function(){
+    if($(this).next() !== false){
+      $(this).removeClass('error').next().remove();
+    }
+  });
