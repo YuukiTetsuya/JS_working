4008501eb2cc44bca9c7b3a743bb5f21ea2ac2a6
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 16:26:57 +0900

    blur,name属性値の取得方法を学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,29 +1,4 @@
-$('.gallery a').bind('click', function(e){
-  // gallery aセレクタクリック時にeパラメータでHTMLよりjQueryを先に適用させる
-  e.preventDefault();
-  // 暗幕を表示させる
-  $('#cover').css({
-    'width':    $(window).width(),
-    'height':    $(window).height()
-  })
-  .show();
-  $('#largeImg')
-  .css({
-    'position': 'absolute',
-    // 画面の中央を求める
-    'left':     Math.floor(($(window).width() - 400) / 2) + 'px',
-    'top':      $(window).scrollTop() + 30 + 'px'
-  })
-　// クリックした大画像のURLを、クリックした自身のsrc属性に変換
-  // find-endで#largeImg要素内のimg要素のsrc属性を変換
-.find('img').attr('src', $(this).attr('href')).end()
-.fadeIn('slow');
-});
-
-// 暗幕,画像をクリック時に暗幕と画像をフェードアウトする
-$('#cover, #largeImg').bind('click', function(){
-  // コールバックで写真をスローでフェードアウト後、暗幕をフェードアウトする
-  $('#largeImg').fadeOut('slow', function(){
-    $('#largeImg, #cover').fadeOut();
-  });
+// input要素のname属性値がmynameをセレクタし、他のコントロールにフォーカスが移ったとき、値を取得する
+$('input[name=myname]').bind('blur', function(){
+  console.log($(this).val());
 });
