e1f240eaff451ed5d720a3b2481be2ccbffd3e5f
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 14:08:45 +0900

    callback機能を学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,26 @@
 $('.gallery a').bind('click', function(e){
+  // gallery aセレクタクリック時にeパラメータでHTMLよりjQueryを先に適用させる
   e.preventDefault();
+  // 暗幕を表示させる
+  $('#cover').css({
+    'width':    $(window).width(),
+    'height':    $(window).height()
+  })
+  .show();
   $('#largeImg')
   .css({
     'position': 'absolute',
     // 画面の中央を求める
-    'left':     Math.floor(($(window.).width() - 400) / 2) + 'px',
+    'left':     Math.floor(($(window).width() - 400) / 2) + 'px',
     'top':      $(window).scrollTop() + 30 + 'px'
   })
-.show();
+.fadeIn();
+});
+
+// 暗幕,画像をクリック時に暗幕と画像をフェードアウトする
+$('#cover, #largeImg').bind('click', function(){
+  // コールバックで写真をスローでフェードアウト後、暗幕をフェードアウトする
+  $('#largeImg').fadeOut('slow', function(){
+    $('#largeImg, #cover').fadeOut();
+  });
 });
