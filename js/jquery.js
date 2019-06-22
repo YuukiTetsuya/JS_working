ca07ede9b209a58c980535c7a02f6b6ad2ad1f26
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 17:32:18 +0900

    next,addClass,removeClassを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,16 @@
 // input要素のname属性値からフォーカスが外れた時に未記入である場合、
-$('input[name=myname]').bind('blur', function(){
+$('input[name=myname]')
+.bind('blur', function(){
   if($(this).val() === ''){
-    // inputの親要素ddの中にappendでpタグを追加
-  $(this).parent().append('<p class="error">&#x26a0;この項目は必ずご記入ください</p>');
-}
+// input要素にerrorクラスをaddし、inputの親要素ddの中にappendでpタグを追加
+  $(this).addClass('error').parent()
+  .append('<p id="error">&#x26a0;この項目は必ずご記入ください</p>');
+  }
+})
+// inputの次の要素pタグ(エラーメッセージ)が空でなければ、removeする
+.bind('focus', function(){
+  if($(this).next() !== false){
+    $(this).removeClass('error')
+    .next().remove();
+  }
 });
