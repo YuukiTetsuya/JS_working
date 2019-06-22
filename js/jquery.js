cc669a121cd31b46ee3ec74080df1e67fdcb83ec
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 18:08:14 +0900

    parentsを学習

--- js/jquery.js
+++ js/jquery.js
@@ -4,7 +4,7 @@ $('input[name=myname]')
   if($(this).val() === ''){
 // input要素にerrorクラスをaddし、inputの親要素ddの中にappendでpタグを追加
   $(this).addClass('error').parent()
-  .append('<p id="error">&#x26a0;この項目は必ずご記入ください</p>');
+  .append('<p class="error">&#x26a0;この項目は必ずご記入ください</p>');
   }
 })
 // inputの次の要素pタグ(エラーメッセージ)が空でなければ、removeする
@@ -12,5 +12,16 @@ $('input[name=myname]')
   if($(this).next() !== false){
     $(this).removeClass('error')
     .next().remove();
+  }
+});
+
+// input要素の属性値から二つ上のdd要素に対し、p要素をappendする
+$('input[name=gender]').parents('dd')
+.append('<p class="error">&#x26a0;この項目は必ずお選びください</p>')
+.end()
+.bind('change', function(){
+// ラジオボタンが選択されていれば、エラーメッセージをremoveする
+  if($('input[name=gender]:checked').val() !== ''){
+    $(this).parents('dd').find('.error').remove();
   }
 });
