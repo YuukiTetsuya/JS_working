b9422fb67fa21fda8046e7b4fac84b33aa6893f1
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 15:58:00 +0900

    複数画像にポップアップを適用

--- js/jquery.js
+++ js/jquery.js
@@ -14,7 +14,9 @@ $('.gallery a').bind('click', function(e){
     'left':     Math.floor(($(window).width() - 400) / 2) + 'px',
     'top':      $(window).scrollTop() + 30 + 'px'
   })
-.fadeIn();
+　// クリックした大画像のURLを、クリックした自身のsrc属性に変換
+$('#largeImg img').attr('src', $(this).attr('href'));
+$('#largeImg').fadeIn('slow');
 });
 
 // 暗幕,画像をクリック時に暗幕と画像をフェードアウトする
