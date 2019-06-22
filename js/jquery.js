e1136ccb3209e6a5c487beeafb0934938e382d74
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-22 16:39:42 +0900

    parent,appendを学習

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,7 @@
-// input要素のname属性値からフォーカスが外れた時に未記入である場合、コントロールをログする
+// input要素のname属性値からフォーカスが外れた時に未記入である場合、
 $('input[name=myname]').bind('blur', function(){
-  if($(this).val() == ""){
-  console.log('未記入です')
-};
+  if($(this).val() === ''){
+    // inputの親要素ddの中にappendでpタグを追加
+  $(this).parent().append('<p class="error">&#x26a0;この項目は必ずご記入ください</p>');
+}
 });
