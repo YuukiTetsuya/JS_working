a10f5f9fdd73b6ea56c1319157e04a30d13ebb58
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-07-06 19:03:30 +0900

    getJSONメソッド作成

--- js/ajax.js
+++ js/ajax.js
@@ -2,13 +2,15 @@ $.ajax({
     url:  'news.json',
     dataType: 'json',
     async:    true,
-    success:  function(xml){
+    success:  function(json){
+      $.getJSON('readnews.php', function(json){)
       var html = '';
       for(i in json){
         html += '<li><a href="' + json[i].link + '">' + json[i].
         title + '</a></li>';
       }
       $('#news').html('<ul>' + html + '</ul>');
+    }
     },
     error:  function(html){
       alert('データの読み込みに失敗しました。しばらくたってからアクセスし直してみてください。');
