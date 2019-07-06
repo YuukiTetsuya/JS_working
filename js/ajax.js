15d2b568db27226ea2d17e248c436415fad11afc
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-07-06 17:35:46 +0900

    xmlでタイトルを読み出し

--- js/ajax.js
+++ js/ajax.js
@@ -1,2 +1,11 @@
-// loadメソッドでajaxでHTMLの内容をID:newsに差し替える
-$('#news').load('news.html');
+$.ajax({
+    url:  'rss.xml',
+    dataType: 'xml',
+    async:    true,
+    success:  function(xml){
+      console.log($(xml).find('channel item:eq(6) title').text());
+    },
+    error:  function(html){
+      alert('データの読み込みに失敗しました。しばらくたってからアクセスし直してみてください。');
+    }
+});
