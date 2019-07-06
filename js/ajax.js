72f9e20d38fd31dbc425bb1745f8d8e0607c4d72
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-07-06 19:08:17 +0900

    jsonファイル作成2

--- js/ajax.js
+++ js/ajax.js
@@ -3,14 +3,12 @@ $.ajax({
     dataType: 'json',
     async:    true,
     success:  function(json){
-      $.getJSON('readnews.php', function(json){)
       var html = '';
       for(i in json){
         html += '<li><a href="' + json[i].link + '">' + json[i].
         title + '</a></li>';
       }
       $('#news').html('<ul>' + html + '</ul>');
-    }
     },
     error:  function(html){
       alert('データの読み込みに失敗しました。しばらくたってからアクセスし直してみてください。');
