4c388873277736f4d871935777ec22d83e4be999
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-07-17 15:08:10 +0900

    タイピングゲーム完成

--- MyTypingGame/js/main.js
+++ MyTypingGame/js/main.js
@@ -2,22 +2,25 @@
 
 {
   const words = [
-    'apple',
-    'sky',
-    'blue',
-    'middle',
-    'set',
+    'Linux',
+    'Apache',
+    'MySQL',
+    'PHP',
+    'LAMP',
   ];
-  // 0 ~ 4(5個)wordsの要素数をランダムにwordに代入
-  let word = words[Math.floor(Math.random() * words.length)];
-  let loc = 0;
-  let score = 0;
-  let miss = 0;
-  const timeLimit = 3 * 1000;
+
+  let word
+  let loc
+  let score
+  let miss
+  const timeLimit = 30 * 1000;
+  let startTime;
+  let isPlaying = false;
 
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
+  const timerLabel = document.getElementById('timer');
 
   function updateTarget(){
     let placeholder = '';
@@ -28,11 +31,64 @@
     target.textContent = placeholder + word.substring(loc);
   }
 
+  function showResult(){
+    const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+    alert(`${score} 正解, ${miss} ミス, ${accuracy.toFixed(2)}% 正解率`);
+  }
+
+  function updateTimer(){
+    // 残り時間を計算
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+    // 10ms毎にupdateTimerを呼び出す
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+    }, 10);
+
+    if(timeLeft < 0){
+      // ゲームを終了させる
+      isPlaying = false;
+
+      clearTimeout(timeoutId);
+      timerLabel.textContent = '0.00';
+      setTimeout(() => {
+        showResult();
+      }, 100);
+
+      timerLabel.textContent = '0.00';
+      target.textContent = '[クリック]でリプレイ';
+    }
+  };
+
   window.addEventListener('click', () => {
+    if(isPlaying === true){
+      return;
+    }
+    // ゲームをスタートさせる
+    isPlaying = true;
+
+// 変数の初期値
+
+    loc = 0;
+    score = 0;
+    miss = 0;
+    scoreLabel.textContent = score;
+    missLabel.textContent = miss;
+
+// n個のwordsの要素数をランダムにwordに代入
+    word = words[Math.floor(Math.random() * words.length)];
+
     updateTarget();
+    startTime = Date.now();
+    updateTimer();
   });
 
   window.addEventListener('keyup', e => {
+    // ゲームが始まっていない場合は以降の処理に進めない
+    if (isPlaying !== true){
+      return;
+    }
+
     console.log(e.key);
     // wordの中の0番目の文字とkeyが一致していたら
     if (e.key === word[loc]) {
